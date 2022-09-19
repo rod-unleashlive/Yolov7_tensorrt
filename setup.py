beff7eda6a3cc7454cc6dce8e5044b8f69d3bee6
rod-unleashlive <rodwin.a@unleashlive.com>
2022-09-19 13:15:05 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+requirements = ['numpy==1.23.3','Pillow==9.2.0','pycuda==2022.1','pytools==2022.1.12','opencv-python==4.5.1.48','nvidia-tensorrt==8.4.3.1']
+
+# setuptools.setup(
+#     name="yolov7_package",
+#     version="0.0.5",
+#     author="Maxim Volkovskiy",
+#     author_email="maxwolf8852@gmail.com",
+#     description="Bindings for yolov7 in one class",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/maxwolf8852/yolov7_package.git",
+#     packages=setuptools.find_packages(),
+#     install_requires=requirements,
+#     classifiers=[
+#         "Programming Language :: Python :: 3.8",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     python_requires='>=3.8',
+# )
+
+setuptools.setup(
+    name='yolov7_tensorrt',
+    version='1.0.0',
+    url='https://github.com/rod-unleashlive/Yolov7_tensorrt.git',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+)
